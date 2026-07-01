1a1fda995d11acde41db8bd141a48ca00756e68f
TRuong Dang To QUYEN <quyentdt0042@ut.edu.vn>
2026-07-02 00:53:54 +0700

    feat: integrate Phase 4 schedule assignment logic and enhance output formatting

--- src/scheduler/ScheduleBuilder.cpp
+++ src/scheduler/ScheduleBuilder.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * @file    ScheduleBuilder.cpp
  * @author  To Quyen
@@ -90,47 +89,4 @@ vector<ScheduleItem> ScheduleBuilder::build(
 
 
 
-=======
-#include "ScheduleBuilder.h"
-#include <map>
-#include <string>
 
-std::vector<FinalScheduleItem> buildScheduleWithRooms(
-    const std::vector<Subject>& subjects,
-    const std::vector<int>& colors,
-    const std::vector<Timeslot>& timeslots,
-    std::vector<Room>& rooms
-) {
-    std::vector<FinalScheduleItem> schedule;
-    std::map<std::string, bool> roomSlotOccupied;
-
-    for (size_t i = 0; i < subjects.size(); ++i) {
-        FinalScheduleItem item;
-        item.subject = subjects[i];
-
-        size_t colorId = static_cast<size_t>(colors[i]);
-        if (colorId < timeslots.size()) {
-            item.slot = timeslots[colorId];
-        } else {
-            item.slot = { "TS_UNKNOWN", "Chua xep ca", "N/A" };
-        }
-
-        Room assignedRoom = { "Chua gan", "N/A", 0 };
-        for (const auto& r : rooms) {
-            if (r.type != item.subject.requiredRoomType) continue;
-            if (r.capacity < item.subject.studentCount) continue;
-
-            std::string key = r.id + "_" + item.slot.id;
-            if (roomSlotOccupied[key]) continue;
-
-            roomSlotOccupied[key] = true;
-            assignedRoom = r;
-            break;
-        }
-
-        item.room = assignedRoom;
-        schedule.push_back(item);
-    }
-    return schedule;
-}
->>>>>>> d5d823b5aba332cec20856debecf02dab6a209a3
--- src/scheduler/ScheduleBuilder.h
+++ src/scheduler/ScheduleBuilder.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * @file    ScheduleBuilder.h
  * @author  To Quyen
@@ -44,28 +43,4 @@ public:
 
 
 
-=======
-#ifndef SCHEDULE_BUILDER_H
-#define SCHEDULE_BUILDER_H
 
-#include <vector>
-#include <string>
-
-// Gọi đúng file duy nhất chứa cấu trúc dữ liệu
-#include "../models/ScheduleData.h"
-
-struct FinalScheduleItem {
-    Subject subject;
-    Timeslot slot;
-    Room room;
-};
-
-std::vector<FinalScheduleItem> buildScheduleWithRooms(
-    const std::vector<Subject>& subjects,
-    const std::vector<int>& colors,
-    const std::vector<Timeslot>& timeslots,
-    std::vector<Room>& rooms
-);
-
-#endif // SCHEDULE_BUILDER_H
->>>>>>> d5d823b5aba332cec20856debecf02dab6a209a3
